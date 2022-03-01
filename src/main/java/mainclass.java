883715bd03e0c4bcdde23b1e1cec6c46f13ff832
sourabhpoddar404 <sourabh.poddar404@gmail.com>
2022-03-01 18:20:35 +0100

    da

--- src/main/java/mainclass.java
+++ src/main/java/mainclass.java
@@ -12,10 +12,10 @@ public static void main(String arg[]) throws UnsupportedEncodingException {
 
     ArrayList<Entity> entity_list = null;
 
-            IndexFiles indexFiles = new IndexFiles();
-            indexFiles.indexttlfile();
-        //SparqlHandler sparqlHandler = new SparqlHandler();
-        //entity_list = sparqlHandler.fetchEntitiesFromFiles();
+            //IndexFiles indexFiles = new IndexFiles();
+           // indexFiles.indexttlfile();
+        SparqlHandler sparqlHandler = new SparqlHandler();
+        entity_list = sparqlHandler.fetchEntitiesFromFiles();
         //entity_list = sparqlHandler.fetchClasses();
         // entity_list = sparqlHandler.fetchProperties();
       // entity_list = sparqlHandler.fetchOntologies();
