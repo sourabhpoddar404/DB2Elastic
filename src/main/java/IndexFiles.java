d33f7c9b19f86c73d5aa3cdfd3d54b991a8bf0d5
sourabhpoddar404 <sourabh.poddar404@gmail.com>
2022-03-02 12:17:53 +0100

    da

--- src/main/java/IndexFiles.java
+++ src/main/java/IndexFiles.java
@@ -43,9 +43,9 @@ public class IndexFiles {
                         new HttpHost("porque.cs.upb.de", 9400, "http")));
 
 
-        String dataFile = "/data-disk/kg-fusion/en/interlanguage_links_en.ttl";
+        String dataFile = "/data-disk/kg-fusion/en/long_abstracts_en.ttl";
 
-        int numoflines = 44122707;
+        int numoflines = 4935281;
         int i = 0;
 
         Map<String, String> fileEntityMap = new LinkedHashMap<>();
